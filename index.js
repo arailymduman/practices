abdd8c93eb62486d323943cac53d5b422f8dd656
arailymduman <99992302+arailymduman@users.noreply.github.com>
2022-02-19 00:57:25 +0600

    JS Practice 1

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
-/*i did this task in concole*/
+document.querySelector("h1").innerHTML = "Arailym CS-2119";
+function display()
+{
+  document.getElementById('date').innerHTML = Date();
+}
+document.querySelector("h3").innerHTML = "876 days left until the freedom!"
+function multiplyBy() {
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML = num1 * num2;
+}
 
-/*1 task*/ const paragraph = document.querySelector('p');
-
-  paragraph.innerHTML = paragraph.innerText
-  .split(' ')
-  .map(word => word.length >= 8 ? `<span style="background: yellow">${word}</span>` : word)
-  .join(' ');
-
-
-/*2 task*/const anchorLink = document.createElement('a');
-anchorLink.innerText = 'Top 8 interview questions';
-anchorLink.href = "https://www.inc.com/jeff-haden/this-new-linkedin-study-reveals-top-8-job-interview-questions-and-how-great-job-candidates-answer-them.html";
-document.body.append(anchorLink);
-
-
-/*3 task*/paragraph.innerHTML = paragraph.innerHTML.split(/\.[^\.|<]/).join('.</p></p>' + '</p>');
-
-
-/*4 task*/const totalWords = paragraph.innerText.split(' ').length;
-const wordCounter = document.createElement('span');
-wordCounter.innerText = `Total Word Counted: ${totalWords}`;
-document.body.insertBefore(wordCounter, paragraph);
-
-
-/*5 task*/Array.from(document.querySelectorAll('p'))
-     .forEach(p => {
-      p.innerHTML = p.innerHTML.replace(/\?/g, ' 🤔');
-      p.innerHTML = p.innerHTML.replace(/\!/g, ' 😲');
-})
+function divideBy() {
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+document.getElementById("result").innerHTML = num1 / num2;
+}
